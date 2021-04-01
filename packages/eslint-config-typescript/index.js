a2c282c04f709b840dae9f0046238a8aa4d3c76b
Luis Nabais <dextro@gmail.com>
2021-04-01 15:34:58 +0100

    Fix typescript config

--- packages/eslint-config-typescript/index.js
+++ packages/eslint-config-typescript/index.js
@@ -1,3 +1,12 @@
+const parentRules = require('@nonsensebb/eslint-config/lib/rules')
+
+const {
+  indent,
+  'no-shadow': noShadow,
+  'no-use-before-define': noUseBeforeDefine,
+  ...rest
+} = parentRules
+
 module.exports = {
   'root': true,
   'parser': '@typescript-eslint/parser',
@@ -25,10 +34,23 @@ module.exports = {
       },
     },
   },
-  'rules': {
+  'rules': Object.assign({}, rest, {
     // TODO: implement a proper naming convention
     '@typescript-eslint/naming-convention': 'off',
-  },
+    'indent': 'off',
+    '@typescript-eslint/indent': indent,
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': [
+      noUseBeforeDefine[0],
+      Object.assign({}, noUseBeforeDefine[1], {
+        'enums': true,
+        'typedefs': true,
+        'ignoreTypeReferences': true,
+      }),
+    ],
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': noShadow
+  }),
   'overrides': [
     {
       'files': [
